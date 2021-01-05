4cb6246a7d9dd814bae1f45bce9d5593471a729d
ferame <alisauskas.j@gmail.com>
2021-01-05 23:04:22 +0000

    The spreadsheet gets read, though it has a very limited upload size, so storing it in local storage should be implemented

--- src/main/java/com/justincode/toad/converter/api/ApiController.java
+++ src/main/java/com/justincode/toad/converter/api/ApiController.java
@@ -1,15 +1,29 @@
 package com.justincode.toad.converter.api;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
-    @PostMapping("/convert/xls/to/xml")
-    public String convertXlsToXml() {
-        return "Converted";
+    @RequestMapping(
+            path = "/convert/xls/to/xml",
+            method = RequestMethod.POST,
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
+    public String convertXlsToXml(@RequestParam("file") MultipartFile file) {
+        String fileName = file.getOriginalFilename();
+        try {
+            Workbook workbook = WorkbookFactory.create(file.getInputStream());
+            return fileName + workbook.getSheetName(0);
+        } catch (IOException e) {
+            return "Issue with file processing";
+        }
     }
 }
